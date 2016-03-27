981ed6857a0b8b0b7dbb36d6331ae01f5334d839
Francisco Puerta <francisco.puerta.puerta@gmail.com>
2016-03-27 23:39:50 +0200

    First Commit

---
+++ Project1/Main.cpp
@@ -0,0 +1,7 @@
+#include <Windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    MessageBox(NULL, "This is going to be funny", "Windows Programming", MB_OK);
+    return 0;
+}
